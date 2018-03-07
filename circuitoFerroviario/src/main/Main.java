5416845e3b06171231fcfbf9aa293c18365b0ad2
chloe <chloe@chloe-lotus>
2018-03-07 05:11:23 -0300

    Politicas e interseccion a revisar

--- circuitoFerroviario/src/main/Main.java
+++ circuitoFerroviario/src/main/Main.java
@@ -3,7 +3,6 @@ package main;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.LinkedHashMap;
 import java.util.Scanner;
 
--- circuitoFerroviario/src/main/Monitor.java
+++ circuitoFerroviario/src/main/Monitor.java
@@ -3,6 +3,7 @@ package main;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Date;
+import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.concurrent.locks.Condition;
 import java.util.concurrent.locks.ReentrantLock;
@@ -15,18 +16,10 @@ public class Monitor {
 	private LinkedHashMap<String, Integer> marcadoInicial;
 	private ArrayList<String> transiciones;
 	private ArrayList<String> recorridoTren;
-	private LinkedHashMap<String, Condition> colaCondicion;
+	private HashMap<String, Condition> colaCondicion;
+	private LinkedHashMap<Integer, String> politicas;
 	
 	private final ReentrantLock lock = new ReentrantLock();
-	
-	/*
-	private final Condition fullTren = lock.newCondition();
-	private final Condition notFullTren = lock.newCondition();
-	private final Condition emptyEstacion = lock.newCondition();
-	private final Condition notFullMaquina = lock.newCondition();
-	private final Condition notFullVagon = lock.newCondition();
-	private final Condition notEmpty = lock.newCondition();
-	*/
 	
 	/*
 	 * 1) Disparo red con el disparo correspondiente a la transicion representada por la condicion a la que se notifico.
@@ -49,23 +42,83 @@ public class Monitor {
 	private final Condition subidaEstacionD = lock.newCondition();
 	private final Condition bajadaEstacionD = lock.newCondition();
 	
-	private final String trenEstacionAEspera = "P13";
-	private final String trenEstacionBEspera = "P7";
-	private final String trenEstacionCEspera = "P10";
-	private final String trenEstacionDEspera = "P37";
+	/* Plazas */
+	
+	private final String trenEstacionAEspera = "ATW";
+	private final String trenEstacionBEspera = "BTW";
+	private final String trenEstacionCEspera = "CTW";
+	private final String trenEstacionDEspera = "DTW";
 
-	private final String trenEstacionAPartida = "P14";
-	private final String trenEstacionBPartida = "P6";
-	private final String trenEstacionCPartida = "P8";
-	private final String trenEstacionDPartida = "P28";
+	private final String trenEstacionAPartida = "ATR";
+	private final String trenEstacionBPartida = "BTR";
+	private final String trenEstacionCPartida = "CTR";
+	private final String trenEstacionDPartida = "DTR";
 
-	private final String trenEstacionAArribo = "P12";
-	private final String trenEstacionBArribo = "P45";
-	private final String trenEstacionCArribo = "P11";
-	private final String trenEstacionDArribo = "P36";
+	private final String trenEstacionAArribo = "RDAT";
+	private final String trenEstacionBArribo = "RABT";
+	private final String trenEstacionCArribo = "RBCT";
+	private final String trenEstacionDArribo = "RCDT";
 
-	private final String vagon = "P18";
-	private final String maquina = "P30";
+	private final String vagon = "VAG";
+	private final String maquina = "MAQ";
+	
+	/* Transiciones */
+	
+	private final String tranTrenEsperandoA = "AW";
+	private final String tranTrenEsperandoB = "BW";
+	private final String tranTrenEsperandoC = "CW";
+	private final String tranTrenEsperandoD = "DW";
+	
+	private final String tranTrenLlenoEstacionVaciaA = "ADe";
+	private final String tranTrenLlenoEstacionVaciaB = "BDe";
+	private final String tranTrenLlenoEstacionVaciaC = "CDe";
+	private final String tranTrenLlenoEstacionVaciaD = "DDe";
+	
+	private final String tranSubidaMaquinaEstacionA = "SMA";
+	private final String tranSubidaMaquinaEstacionB = "SMB";
+	private final String tranSubidaMaquinaEstacionC = "SMC";
+	private final String tranSubidaMaquinaEstacionD = "SMD";
+
+	private final String tranSubidaVagonEstacionA = "SVA";
+	private final String tranSubidaVagonEstacionB = "SVB";
+	private final String tranSubidaVagonEstacionC = "SVC";
+	private final String tranSubidaVagonEstacionD = "SVD";
+
+	
+	private final String tranBajadaMaquinaBEstacionA = "BAMB";
+	private final String tranBajadaMaquinaCEstacionA = "BAMC";
+	private final String tranBajadaMaquinaDEstacionA = "BAMD";
+
+	private final String tranBajadaVagonBEstacionA = "BAVB";
+	private final String tranBajadaVagonCEstacionA = "BAVC";
+	private final String tranBajadaVagonDEstacionA = "BAVD";
+
+	
+	private final String tranBajadaMaquinaAEstacionB = "BBMA";
+	private final String tranBajadaMaquinaCEstacionB = "BBMC";
+	private final String tranBajadaMaquinaDEstacionB = "BBMD";
+
+	private final String tranBajadaVagonAEstacionB = "BBVA";
+	private final String tranBajadaVagonCEstacionB = "BBVC";
+	private final String tranBajadaVagonDEstacionB = "BBVD";
+
+	
+	private final String tranBajadaMaquinaAEstacionC = "BCMA";
+	private final String tranBajadaMaquinaBEstacionC = "BCMB";
+	private final String tranBajadaMaquinaDEstacionC = "BCMD";
+
+	private final String tranBajadaVagonAEstacionC = "BCVA";
+	private final String tranBajadaVagonBEstacionC = "BCVB";
+	private final String tranBajadaVagonDEstacionC = "BCVD";
+
+	
+	private final String tranBajadaMaquinaAEstacionD = "BDMA";
+	private final String tranBajadaMaquinaBEstacionD = "BDMB";
+	private final String tranBajadaMaquinaCEstacionD = "BDMC";
+
+	private final String tranBajadaVagonAEstacionD = "BDVA";
+	private final String tranBajadaVagonBEstacionD = "BDVB";
+	private final String tranBajadaVagonCEstacionD = "BDVC";
 
 	
 	public Monitor(Integer lugaresMaquina, Integer lugaresVagon, Integer[][] matrizMas, Integer[][] matrizMenos, LinkedHashMap<String, Integer> marcado, ArrayList<String> transiciones) {
@@ -81,8 +134,111 @@ public class Monitor {
 		this.matrizMenos = matrizMenos;
 		this.recorridoTren = secuenciaTransiciones();
 		
-		this.colaCondicion = new LinkedHashMap<>();
-		this.colaCondicion.put(this.transiciones.get(0), fullTrenOrEmptyEstacion);
+		this.colaCondicion = new HashMap<>();
+		this.colaCondicion.put(tranTrenEsperandoA, tiempoDeEspera);
+		this.colaCondicion.put(tranTrenEsperandoB, tiempoDeEspera);
+		this.colaCondicion.put(tranTrenEsperandoC, tiempoDeEspera);
+		this.colaCondicion.put(tranTrenEsperandoD, tiempoDeEspera);
+		
+		this.colaCondicion.put(tranTrenLlenoEstacionVaciaA, fullTrenOrEmptyEstacion);
+		this.colaCondicion.put(tranTrenLlenoEstacionVaciaB, fullTrenOrEmptyEstacion);
+		this.colaCondicion.put(tranTrenLlenoEstacionVaciaC, fullTrenOrEmptyEstacion);
+		this.colaCondicion.put(tranTrenLlenoEstacionVaciaD, fullTrenOrEmptyEstacion);
+		
+		
+		this.colaCondicion.put(tranSubidaMaquinaEstacionA, subidaEstacionA);
+		this.colaCondicion.put(tranSubidaVagonEstacionA, subidaEstacionA);
+		
+		this.colaCondicion.put(tranSubidaMaquinaEstacionB, subidaEstacionB);
+		this.colaCondicion.put(tranSubidaVagonEstacionB, subidaEstacionB);
+		
+		this.colaCondicion.put(tranSubidaMaquinaEstacionC, subidaEstacionC);
+		this.colaCondicion.put(tranSubidaVagonEstacionC, subidaEstacionC);
+		
+		this.colaCondicion.put(tranSubidaMaquinaEstacionD, subidaEstacionD);
+		this.colaCondicion.put(tranSubidaVagonEstacionD, subidaEstacionD);
+		
+		
+		this.colaCondicion.put(tranBajadaMaquinaBEstacionA, bajadaEstacionA);
+		this.colaCondicion.put(tranBajadaMaquinaCEstacionA, bajadaEstacionA);
+		this.colaCondicion.put(tranBajadaMaquinaDEstacionA, bajadaEstacionA);
+		this.colaCondicion.put(tranBajadaVagonBEstacionA, bajadaEstacionA);
+		this.colaCondicion.put(tranBajadaVagonCEstacionA, bajadaEstacionA);
+		this.colaCondicion.put(tranBajadaVagonDEstacionA, bajadaEstacionA);
+		
+		this.colaCondicion.put(tranBajadaMaquinaAEstacionB, bajadaEstacionB);
+		this.colaCondicion.put(tranBajadaMaquinaCEstacionB, bajadaEstacionB);
+		this.colaCondicion.put(tranBajadaMaquinaDEstacionB, bajadaEstacionB);
+		this.colaCondicion.put(tranBajadaVagonAEstacionB, bajadaEstacionB);
+		this.colaCondicion.put(tranBajadaVagonCEstacionB, bajadaEstacionB);
+		this.colaCondicion.put(tranBajadaVagonDEstacionB, bajadaEstacionB);
+		
+		this.colaCondicion.put(tranBajadaMaquinaAEstacionC, bajadaEstacionC);
+		this.colaCondicion.put(tranBajadaMaquinaBEstacionC, bajadaEstacionC);
+		this.colaCondicion.put(tranBajadaMaquinaDEstacionC, bajadaEstacionC);
+		this.colaCondicion.put(tranBajadaVagonAEstacionC, bajadaEstacionC);
+		this.colaCondicion.put(tranBajadaVagonBEstacionC, bajadaEstacionC);
+		this.colaCondicion.put(tranBajadaVagonDEstacionC, bajadaEstacionC);
+		
+		this.colaCondicion.put(tranBajadaMaquinaAEstacionD, bajadaEstacionD);
+		this.colaCondicion.put(tranBajadaMaquinaBEstacionD, bajadaEstacionD);
+		this.colaCondicion.put(tranBajadaMaquinaCEstacionD, bajadaEstacionD);
+		this.colaCondicion.put(tranBajadaVagonAEstacionD, bajadaEstacionD);
+		this.colaCondicion.put(tranBajadaVagonBEstacionD, bajadaEstacionD);
+		this.colaCondicion.put(tranBajadaVagonCEstacionD, bajadaEstacionD);
+		
+		
+		
+		this.politicas = new LinkedHashMap<>();
+		int index = 0;
+		this.politicas.put(index++, tranBajadaMaquinaAEstacionD);
+		this.politicas.put(index++, tranBajadaMaquinaBEstacionA);
+		this.politicas.put(index++, tranBajadaMaquinaCEstacionB);
+		this.politicas.put(index++, tranBajadaMaquinaDEstacionC);
+		this.politicas.put(index++, tranBajadaVagonAEstacionD);
+		this.politicas.put(index++, tranBajadaVagonBEstacionA);
+		this.politicas.put(index++, tranBajadaVagonCEstacionB);
+		this.politicas.put(index++, tranBajadaVagonDEstacionC);
+		
+		
+		this.politicas.put(index++, tranBajadaMaquinaAEstacionB);
+		this.politicas.put(index++, tranBajadaMaquinaAEstacionC);
+		this.politicas.put(index++, tranBajadaMaquinaBEstacionC);
+		this.politicas.put(index++, tranBajadaMaquinaBEstacionD);
+		this.politicas.put(index++, tranBajadaMaquinaCEstacionA);
+		this.politicas.put(index++, tranBajadaMaquinaCEstacionD);
+		this.politicas.put(index++, tranBajadaMaquinaDEstacionA);
+		this.politicas.put(index++, tranBajadaMaquinaDEstacionB);
+		this.politicas.put(index++, tranBajadaVagonAEstacionB);
+		this.politicas.put(index++, tranBajadaVagonAEstacionC);
+		this.politicas.put(index++, tranBajadaVagonBEstacionC);
+		this.politicas.put(index++, tranBajadaVagonBEstacionD);
+		this.politicas.put(index++, tranBajadaVagonCEstacionA);
+		this.politicas.put(index++, tranBajadaVagonCEstacionD);
+		this.politicas.put(index++, tranBajadaVagonDEstacionA);
+		this.politicas.put(index++, tranBajadaVagonDEstacionB);
+		
+		
+		this.politicas.put(index++, tranSubidaMaquinaEstacionA);
+		this.politicas.put(index++, tranSubidaMaquinaEstacionB);
+		this.politicas.put(index++, tranSubidaMaquinaEstacionC);
+		this.politicas.put(index++, tranSubidaMaquinaEstacionD);
+		this.politicas.put(index++, tranSubidaVagonEstacionA);
+		this.politicas.put(index++, tranSubidaVagonEstacionB);
+		this.politicas.put(index++, tranSubidaVagonEstacionC);
+		this.politicas.put(index++, tranSubidaVagonEstacionD);
+		
+		
+		this.politicas.put(index++, tranTrenLlenoEstacionVaciaA);
+		this.politicas.put(index++, tranTrenLlenoEstacionVaciaB);
+		this.politicas.put(index++, tranTrenLlenoEstacionVaciaC);
+		this.politicas.put(index++, tranTrenLlenoEstacionVaciaD);
+		
+		this.politicas.put(index++, tranTrenEsperandoA);
+		this.politicas.put(index++, tranTrenEsperandoB);
+		this.politicas.put(index++, tranTrenEsperandoC);
+		this.politicas.put(index++, tranTrenEsperandoD);
+
 	}
 	
 	public void abordarTren() throws InterruptedException {
@@ -194,17 +350,101 @@ public class Monitor {
 			}
 			System.out.println(" ");
 			
-			if(!dispararRed(vectorDisparo)) {
-				return;
+
+			LinkedHashMap<String, Boolean> preInterseccion = getInterseccionCondicion(getSensibilizadas(), lock);
+			
+
+			String[] prioritariasPrevias = politicas.values().toArray(new String[0]);
+			for(String transicion: prioritariasPrevias) {
+				if(preInterseccion.get(transicion)) {
+					dispararRed(transicion);
+					break;
+				}
 			}
 			
-			LinkedHashMap<String, Boolean> vectorSensibilizadas = getSensibilizadas();
+			ArrayList<String> transicionesRemanentes = new ArrayList<String>(this.transiciones);
+			for(String iterada: prioritariasPrevias) {
+				transicionesRemanentes.remove(transicionesRemanentes.indexOf(iterada));
+			}
+			
+			for(String transicionInmediata: transicionesRemanentes) {
+				if(preInterseccion.get(transicionInmediata)) {
+					dispararRed(transicionInmediata);
+					break;
+				}
+			}
+			
+			
+//			if(!dispararRed(vectorDisparo)) {
+//				return;
+//			}
+			
+			
+			
+			
+			LinkedHashMap<String, Boolean> vectorInterseccion = getInterseccionCondicion(getSensibilizadas(), lock);
+			
+
+			String[] prioritarias = politicas.values().toArray(new String[0]);
+			for(String transicion: prioritarias) {
+				if(vectorInterseccion.get(transicion)) {
+					colaCondicion.get(transicion).notify();
+					return;
+				}
+			}
+			
+			ArrayList<String> transicionesRestantes = new ArrayList<String>(this.transiciones);
+			for(String iterada: prioritarias) {
+				transicionesRestantes.remove(transicionesRestantes.indexOf(iterada));
+			}
+			
+			for(String transicionInmediata: transicionesRestantes) {
+				if(vectorInterseccion.get(transicionInmediata)) {
+//					dispararRed(transicionInmediata);
+					return;
+				}
+			}
+			
+//			int transicionesADisparar = 0;
+//			for(String transicion: transiciones) {
+//				if(vectorInterseccion.get(transicion)) {
+//					transicionesADisparar++;
+//				}
+//			}
+//			
+//			if(transicionesADisparar > 1) {
+//				colaCondicion.get(politicas(vectorInterseccion)).notify();
+//			} else {
+//				
+//			}
 			
 			
 			
 		} finally {
 			lock.unlock();
 		}
+	}
+	
+	private String politicas(LinkedHashMap<String, Boolean> interseccion) {
+		String[] prioritarias = politicas.keySet().toArray(new String[0]);
+		
+		for(String transicion: prioritarias) {
+			if(interseccion.get(transicion)) {
+				return transicion;
+			}
+		}
+
+		return "";
+	}
+	
+	private LinkedHashMap<String, Boolean> getInterseccionCondicion(LinkedHashMap<String, Boolean> vectorSensibilizadas, ReentrantLock lock) {
+		LinkedHashMap<String, Boolean> interseccion = new LinkedHashMap<>();
+		
+		for(String transicion: this.transiciones) {
+			interseccion.put(transicion, vectorSensibilizadas.get(transicion) && (colaCondicion.containsKey(transicion)? lock.getWaitQueueLength(colaCondicion.get(transicion)) != 0 : true));
+		}
+		
+		return interseccion;
 	}
 	
 	private static ArrayList<String> secuenciaTransiciones(){
@@ -250,8 +490,9 @@ public class Monitor {
 	}
 	
 	private boolean dispararRed(String transicion) {
+		System.out.println(transicion);
 		Integer[] vectorDisparo = Collections.nCopies(transiciones.size(), 0).toArray(new Integer[0]);
-		vectorDisparo[transicion.indexOf(transicion)] = 1;
+		vectorDisparo[transiciones.indexOf(transicion)] = 1;
 		
 		return dispararRed(vectorDisparo);
 	}
